5be896c2bfbe96cf8a631ed4d2f1650b6ccd94f9
MIkhail <famm93@mail.ru>
2023-03-27 19:00:11 +0300

    2-dice

--- app.js
+++ app.js
@@ -18,3 +18,57 @@ const Res = []
 
 
 console.log(Res);
+
+
+
+
+const DICES = {
+    
+
+    d4: {
+       name: 'd4',
+       faceAmount: 4,
+   },
+   d6: {
+       name: 'd6',
+       faceAmount: 6,
+   },
+   d8: {
+       name: 'd8',
+       faceAmount: 8,
+   },
+   d10: {
+       name: 'd10',
+       faceAmount: 10,
+   },
+   d12: {
+       name: 'd12',
+       faceAmount: 12,
+   },
+   d16: {
+       name: 'd16',
+       faceAmount: 16,
+   },
+   d20: {
+       name: 'd20',
+       faceAmount: 20,
+   },
+
+
+}
+
+
+function play(dice) {
+
+console.log(`${(Math.floor(Math.random() * 6) + 1)}${dice.name}`)
+
+}
+play(DICES.d4)
+
+
+function play1(dice) {
+
+console.log(`${dice.faceAmount}${dice.name}`)
+
+}
+play1(DICES.d10)
